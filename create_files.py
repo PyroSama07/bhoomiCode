6b21f332baf631c2efb6d33ada59fb2074b5faa7
Rochak Ranjan <rochakranjan1011@gmail.com>
2024-02-09 05:03:08 +0530

    changed create_files

--- create_files.py
+++ create_files.py
@@ -3,8 +3,9 @@ import os
 
 os.mkdir('pincodes/')
 pincode = {}
-for i in range(0,29_999,10):
-    string = f'pincodes/pincode.{str(i)}_{str(i+9)}.pkl'
+pincode_in_one_file = 10
+for i in range(0,29_999,pincode_in_one_file):
+    string = f'pincodes/pincode.{str(i)}_{str(i+pincode_in_one_file-1)}.pkl'
     with open(string,'wb') as f:
         pickle.dump(pincode,f)
         f.close()
