31822735d7a6271ab1fb2e248cf7aec25d270e9d
Rochak Ranjan <rochakranjan1011@gmail.com>
2024-02-10 23:02:23 +0530

    create_file update

--- utils/create_files.py
+++ utils/create_files.py
@@ -18,3 +18,17 @@ empty_dict = {}
 with open('pincodes/first_open.pkl','wb') as f:
     pickle.dump(empty_dict,f)
     f.close()
+
+def get_sellers(x):
+    seller_ids = random.sample(range(10_000_000),x)
+    pincode = {}
+    for i in range (x):
+        pincode[seller_ids[i]]=random.randint(1,5)
+    return pincode
+
+pincodes = {}
+for i in range(10):
+    pincodes[i]=get_sellers(20)
+with open('pincodes/pincode.0_9.pkl', 'wb') as f:
+    pickle.dump(pincodes, f)
+    f.close()
